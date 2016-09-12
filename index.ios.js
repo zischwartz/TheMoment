ab71587dfa1bc1b98adb3cba22de9cb9cec95fc4
Zach Schwartz <zischwartz@gmail.com>
2016-09-12 14:16:41 -0400

    camera works, disabled preview

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,68 @@
-import { AppRegistry } from 'react-native';
+// import { AppRegistry } from 'react-native';
+//
+// import TheMomentApp from './components/TheMomentApp';
+//
+// AppRegistry.registerComponent('TheMoment', () => TheMomentApp);
 
-import TheMomentApp from './components/TheMomentApp';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
+import Camera from 'react-native-camera';
 
-AppRegistry.registerComponent('TheMoment', () => TheMomentApp);
+class BadInstagramCloneApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          type={Camera.constants.Type.front}
+          aspect={Camera.constants.Aspect.fill}>
+
+        </Camera>
+        <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+
+      </View>
+    );
+  }
+
+  takePicture() {
+    this.camera.capture()
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  preview: {
+    height: 0,
+    width: 0
+    // flex: 1,
+    // justifyContent: 'flex-end',
+    // alignItems: 'center',
+    // height: Dimensions.get('window').height,
+    // width: Dimensions.get('window').width,
+    // display: 'none'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
+
+AppRegistry.registerComponent('TheMoment', () => BadInstagramCloneApp);
